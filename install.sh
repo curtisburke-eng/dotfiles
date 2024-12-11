37d129e0843bd2c1ca37a331e63fba70e44637e0
Curtis Burke <105394286+curtisburke-eng@users.noreply.github.com>
2024-12-11 14:07:20 -0700

    Fix dest path for oh-my-zsh theme

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt install zsh -y
 chsh -s $(which zsh)
 sudo apt install wget -y
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh --no-check-certificate)" -y
-cp ~/dotfiles/refined-edited.zsh-theme .oh-my-zsh/themes/refined-edited.zsh-theme
+cp ~/dotfiles/refined-edited.zsh-theme ~/.oh-my-zsh/themes/refined-edited.zsh-theme
 cp ~/dotfiles/.zshrc ~/.zshrc
 cp ~/dotfiles/.dircolors ~/.dircolors
 git config --global --add safe.directory /data  
