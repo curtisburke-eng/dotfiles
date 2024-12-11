fc5301b6734408ad82cfe64b950dfff4fa96374d
curtisburke-eng <curtisburke.eng@gmail.com>
2024-12-11 13:55:18 -0700

    Copy dotfiles to home dir

--- install.sh
+++ install.sh
@@ -3,3 +3,10 @@
 sudo apt update 
 sudo apt install zsh -y
 chsh -s $(which zsh)
+sudo apt install wget -y
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh --no-check-certificate)"
+cp ~/dotfiles/refined-edited.zsh-theme .oh-my-zsh/themes/refined-edited.zsh-theme
+cp ~/dotfiles/.zshrc
+cp ~/dotfiles/.dircolors
+git config --global --add safe.directory /data  
+zsh
