fc3fc12a364ec8666beed3c31e6c840b2a2df8cb
Curtis Burke <105394286+curtisburke-eng@users.noreply.github.com>
2024-12-11 14:03:39 -0700

    Add destinations to cp commands

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ sudo apt update
 sudo apt install zsh -y
 chsh -s $(which zsh)
 sudo apt install wget -y
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh --no-check-certificate)"
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh --no-check-certificate)" -y
 cp ~/dotfiles/refined-edited.zsh-theme .oh-my-zsh/themes/refined-edited.zsh-theme
-cp ~/dotfiles/.zshrc
-cp ~/dotfiles/.dircolors
+cp ~/dotfiles/.zshrc ~/.zshrc
+cp ~/dotfiles/.dircolors ~/.dircolors
 git config --global --add safe.directory /data  
 zsh
