77722a1297495cc7bf4ac33df32b573382dea29c
curtisburke-eng <curtisburke.eng@gmail.com>
2025-04-15 13:26:19 -0600

    Add ripgrep pkg

--- 1-install-pkgs.sh
+++ 1-install-pkgs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Install packages
-sudo apt update 
+sudo apt update
 sudo apt-get update
 sudo apt install curl -y
 sudo apt install htop
 sudo apt install net-tools
 sudo apt-get install neofetch
 sudo apt install flameshot
+sudo apt install ripgrep
 
 neofetch
